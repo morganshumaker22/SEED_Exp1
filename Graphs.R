b8a5b65b21053d6ecc7cd81f40f84cb36adf3986
morganshumaker22 <m.shumaker@tcu.edu>
2022-08-01 16:22:59 -0500

    fix?

--- Graphs.R
+++ Graphs.R
@@ -15,11 +15,18 @@ Exp1_Graphics <- Exp1_Graphics %>%
   mutate(accuracy = as.numeric(Accuracy))
 
 ggplot(Exp1_Graphics, aes(y=accuracy, x=condition, fill = condition)) + 
-  geom_bar(stat= "identity", width = 1)+ facet_wrap(~AgeGroup2)+ylim(0,1)+
+  geom_bar(stat= "identity", width = 1, color = "black")+ facet_wrap(~AgeGroup2)+ylim(0,1)+
   theme_apa(base_size = 12, base_family = "")+
-  scale_fill_manual(values=c("black", "grey"))+
+  scale_fill_manual(values=c("black", "white"))+
   labs(x = "Study Condition", y = "Proportion of Correctly Recalled Words")+
-  theme(legend.position="none")
+  theme(legend.position="none")+
+  theme(axis.line = element_line(colour = "black", size = .3))+
+  theme(panel.grid.major = element_blank())+
+  theme(panel.grid.minor = element_blank())+
+  theme(text = element_text(size = 12, family = "Arial", color = "black"))+
+  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)))+
+  theme(panel.spacing.x = unit(0, "null"))+
+  theme(plot.title = element_text(size=12))
 
 
 MetaGraph <- read_csv("MetaGraph.csv")
@@ -28,10 +35,20 @@ MetaGraph <- MetaGraph %>%
   mutate(Estimate = fct_recode(Better_Performance, Estimated = "Predicted"))
 
 ggplot(MetaGraph, aes(x = Condition, y = Proportion, fill = Estimate))+
-  geom_col(position = "fill")+facet_wrap(~Age_Group)+ylim(0,1)+
+  geom_col(position = "fill", color = "black")+facet_wrap(~Age_Group)+ylim(0,1)+
   theme_apa(base_size = 12, base_family = "")+
-  scale_fill_manual(values=c("black", "grey"))+
+  scale_fill_manual(values=c("black", "white"))+
   labs(x = "Study Condition", y = "Proportion", 
-       fill = "Performance")
+       fill = "Performance")+
+  theme(axis.line = element_line(colour = "black", size = .3))+
+  theme(panel.grid.major = element_blank())+
+  theme(panel.grid.minor = element_blank())+
+  theme(text = element_text(size = 12, family = "Arial", color = "black"))+
+  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)))+
+  theme(panel.spacing.x = unit(0, "null"))+
+  theme(plot.title = element_text(size=12))
   
+
+
+
 
